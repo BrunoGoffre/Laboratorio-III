7a8a8f9463586e287d614400fcfd51ebbcc7f7d5
unknown <Brunogoffredo22@gmail.com>
2020-09-10 21:01:49 -0300

    limpieza

--- Logica.js
+++ Logica.js
@@ -1,8 +1,9 @@
-window.onload = inciar;
+window.addEventListener("load",inciar);
 
 function inciar() {
-    document.getElementById("usuario").addEventListener("keypress", Enter);
-    document.getElementById("contraseña").addEventListener("keypress", Enter);
+    document.getElementById("nombre").addEventListener("keypress", Enter);
+    document.getElementById("apellido").addEventListener("keypress", Enter);
+    document.getElementById("btnGuardar").addEventListener("click",AgregarProducto);
 }
 
  function Validar(){
@@ -12,24 +13,42 @@ function inciar() {
     if (usuario.value != "user" || contraseña.value != "123"){
         alert("usuario o contraseña incorrecto");
         contraseña.value="";
+        document.getElementById("ancla").addEventListener("click","");
     }
     else{
         alert("Inicio de sesion existoso");
     }
 }
 
-function Enter(e){
-    
-        var usuario = document.getElementById("usuario").value;
-        var contrañsea = document.getElementById("contraseña");
+function Enter(e){    
         if (e.keyCode == 13)
         {	
             Validar();
         }    
 }
 
+function AgregarProducto(e)
+{
+	var nombre = document.getElementById("nombre").value;
+	var apellido = document.getElementById("apellido").value;
 
-function Limpiar(){
-    document.getElementById("contraseña").value = "";
-    document.getElementById("usuario").value = "";           
+	var tr = document.createElement("tr");
+	tr.setAttribute("id", "Nombre");	
+	
+	var td2 = document.createElement("td");
+	var nodeNombre = document.createTextNode(nombre);
+	td2.appendChild(nodeNombre);
+	tr.appendChild(td2);
+	
+	var td3 = document.createElement("td");
+	var nodeApellido = document.createTextNode(apellido);
+	td3.appendChild(nodeApellido);
+    tr.appendChild(td3);
+    
+    var td4 = document.createElement("td");
+    var nodeAccion = document.createTextNode("Borrar");
+    td4.appendChild(nodeAccion);
+    tr.appendChild(td4);
+
+	tCuerpo.appendChild(tr);
 }
